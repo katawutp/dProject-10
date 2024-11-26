756092d0998f5c2e5ddae6bc1e1c6c24de178517
katawutp <katawut.paramee@gmail.com.com>
2024-11-26 19:14:44 +0700

    fix NextResponse Error

--- src/app/api/stripe-intent/route.ts
+++ src/app/api/stripe-intent/route.ts
@@ -1,4 +1,30 @@
 import Stripe from "stripe";
+import { NextResponse } from 'next/server';
+
+import { NextResponse } from 'next/server';
+
+const { STRIPE_SECRET_KEY } = process.env;
+
+export async function POST(req: Request) {
+    if (!STRIPE_SECRET_KEY) {
+        throw new Error('Stripe secret key not found');
+    }
+
+    const stripe = require('stripe')(STRIPE_SECRET_KEY);
+    const { buyerWalletAddress } = await req.json();
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1000,
+        currency: "thb",
+        description: "คูปองใช้งาน KokKok dApp ในรูปแบบของ NFT สินทรัพย์ดิจิทัล",
+        payment_method_types: ["card"],
+        metadata: { buyerWalletAddress },
+    });
+
+    return NextResponse.json({
+        clientSecret: paymentIntent.client_secret,
+    });
+}
 
 const { STRIPE_SECRET_KEY } = process.env;
 
