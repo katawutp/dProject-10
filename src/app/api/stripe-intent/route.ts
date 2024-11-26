b4ed85a89ca8705e7711a00d93278e459f4b9c2d
katawutp <katawut.paramee@gmail.com.com>
2024-11-26 19:25:13 +0700

    Remove the duplicate definition of STRIPE_SECRET_KEY:

--- src/app/api/stripe-intent/route.ts
+++ src/app/api/stripe-intent/route.ts
@@ -1,5 +1,3 @@
-import Stripe from "stripe";
-
 import { NextResponse } from 'next/server';
 
 const { STRIPE_SECRET_KEY } = process.env;
@@ -23,37 +21,4 @@ export async function POST(req: Request) {
     return NextResponse.json({
         clientSecret: paymentIntent.client_secret,
     });
-}
-
-const { STRIPE_SECRET_KEY } = process.env;
-
-export async function POST(req: Request) {
-    if(!STRIPE_SECRET_KEY){
-        throw new Error('Stripe secret key not found');
-    }
-
-    const { buyerWalletAddress } = await req.json();
-    if (!buyerWalletAddress){
-        throw new Error('Buyer wallet address not found');
-    }
-
-    const stripe = new Stripe(
-        process.env.STRIPE_SECRET_KEY as string,
-        {
-            apiVersion: "2024-11-20.acacia"    
-        }
-         
-    );
-
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: 10_00,
-        currency: "usd",
-        description: "คูปองใช้งาน KokKok dApp ในรูปแบบของ NFT สินทรัพย์ดิจิทัล",
-        payment_method_types: ["card"],
-        metadata: { buyerWalletAddress }
-    });
-
-    return NextResponse.json({
-        clientSecret: paymentIntent.client_secret,
-    })
 }
